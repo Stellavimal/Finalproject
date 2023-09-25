a209051185a1da5f0754c8355eec6030d9fadf23
Lenovo <Lenovo@DESKTOP-4L3DSQ5>
2023-09-25 21:29:52 +0530

    adding new code

--- electionproject/src/App.js
+++ electionproject/src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 // import Header from './components/Header'
-//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/home'
 import Login from './pages/login'
 import Navbar from './pages/navbar'
@@ -19,7 +19,7 @@ function App() {
         {/* <Route path="/contact" element={<Contact />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} /> */}
-     {/* </Routes> 
+      {/* </Routes> 
     </BrowserRouter>    */}
     </>
   )
--- electionproject/src/pages/election.js
+++ electionproject/src/pages/election.js
@@ -1,83 +1,64 @@
 import axios from 'axios';
 import { useState } from 'react';
-import { Form, Row, Col, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 const Addelection = () => {
-const[election, setelection]=useState([])
-const[addelection,setaddelection]=useState({electionname:"",electiondate:"",starttime:"",endtime:"", electiontype:""})
+    const [election, setelection] = useState([])
+    const [addnewelection, setaddelection] = useState({ electionname: "", electiondate: "", electiontype: "", starttime: "", endtime: "", })
+    const [validationError, setValidationError] = useState('');
 
-const adddata=async()=>{
-    const response= await axios.post('/api/addelection/', addelection);
-    console.log("response:",response.data)
-}
-const input = (event) => {
-    const { name, value } = event.target;
-    setaddelection({ ...addelection, [name]: value });
-};
- const haandleSubmit=(event)=>{
-    event.preventDefault()
- }
+    const adddata = async () => {
+        const response = await axios.post('/api/addelection/', addnewelection);
+        setelection([...election, response.data])
+        setaddelection({ electionname: "", electiondate: "", electiontype: "", starttime: "", endtime: "" })
+        console.log("response:", response.data)
+    }
+    // const getdata=async()=>{
+    //     const response1= await axios.get('/api/addelection/');
+    //     console.log("response:",response1.data)
+    // }
+    const input = (event) => {
+        const { name, value } = event.target;
+        setaddelection({ ...addnewelection, [name]: value });
+    };
+
+    const isStartTimeLessThanEndTime = () => {
+        if (addnewelection.starttime && addnewelection.endtime) {
+            const startTimeObj = new Date(`2023-09-25T${addnewelection.starttime}`);
+            const endTimeObj = new Date(`2023-09-25T${addnewelection.endtime}`);
+
+            return startTimeObj < endTimeObj;
+        }
+        return true; // If either startTime or endTime is not provided, consider it valid.
+    };
+    const haandleSubmit = (event) => {
+        event.preventDefault()
+    }
+  
+    const isValid = isStartTimeLessThanEndTime();
+    
     return (<>
+        {!isValid && <p style={{ color: 'red' }}>Start time must be less than End time.</p>}
+        {validationError && <p style={{ color: 'red' }}>{validationError}</p>}
+        <form style={{ textAlign: 'center' }} onSubmit={haandleSubmit}>
+            <label>Election Name:</label>
+            <input type="text" className="form-group mx-sm-3 mb-2" placeholder="Election Name" name='electionname' value={addnewelection.electionname} onChange={input} required /><br />
+            <label>Eelection Date:</label>
+            <input type="date" className="form-group mx-sm-3 mb-2" placeholder="Election Date" name='electiondate' value={addnewelection.electiondate} onChange={input} required /><br />
+            <label>Election Type:</label>
+            <select required name='electiontype' value={addnewelection.electiontype} onChange={input} className="form-group mx-sm-3 mb-2">
+                <option value="" disabled selected>--select Election Type--</option>
+                <option value="GeneralElection">General Election</option>
+                <option value="By-Election">By-Election</option>
+            </select><br />
+            <label>Start Time:</label>
+            <input type="time" placeholder="Enter Time" name='starttime' className="form-group mx-sm-3 mb-2" value={addnewelection.starttime} onChange={input} required /><br />
+            <label>End Time:</label>
+            <input type="time" placeholder="Enter Time" name='endtime' className="form-group mx-sm-3 mb-2" value={addnewelection.endtime} onChange={input} required /><br />
+            {/* <Button type="submit" onClick={getdata}>get</Button> */}
+            <Button variant="primary" onClick={adddata}>Submit</Button>
+        </form>
 
-        <Form style={{ textAlign: 'center' }} onSubmit={haandleSubmit}>
-            <Form.Group className="mb-3">
-                <Row>
-                    <Col xs={2}>
-                        <Form.Label>Election Name</Form.Label>
-                    </Col>
-                    <Col>
-                        <Form.Control type="text" placeholder="Election Name"  name='electionname' onChange={input} required/>
-                    </Col>
-                </Row>
-            </Form.Group>
-            <Form.Group className="mb-3">
-                <Row>
-                    <Col xs={2}>
-                        <Form.Label>Eelection Date</Form.Label>
-                    </Col>
-                    <Col >
-                        <Form.Control type="date" placeholder="Election Date" name='electiondate' onChange={input} required />
-                    </Col>
-                </Row>
-            </Form.Group>
-            <Form.Group className="mb-3">
-                <Row>
-                    <Col xs={2}>
-                        <Form.Label>Start Time</Form.Label>
-                    </Col>
-                    <Col >
-                        <Form.Control type="time" placeholder="Enter Time" name='starttime' onChange={input} required/>
-                    </Col>
-                </Row>
-            </Form.Group>
-            <Form.Group className="mb-3">
-                <Row>
-                    <Col xs={2}>
-                        <Form.Label>End Time</Form.Label>
-                    </Col>
-                    <Col >
-                        <Form.Control type="time" placeholder="Enter Time" name='endtime' onChange={input} required/>
-                    </Col>
-                </Row>
-            </Form.Group>
-            <Form.Group className="mb-3">
-                <Row>
-                    <Col xs={2}>
-                        <Form.Label>Election Type</Form.Label>
-                    </Col>
-                    <Col>
-                        <Form.Select required name='electiontype' onChange={input}>
-                            <option disabled>--select Election Type--</option>
-                            <option>General Election</option>
-                            <option>By-Election</option>
-                        </Form.Select>
-                    </Col>
-                </Row>
-            </Form.Group>
-        
-            <Button type="submit" onClick={adddata}>Submit</Button>
-
-        </Form>
 
     </>);
 }
